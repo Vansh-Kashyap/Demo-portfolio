ffc96379857303a6de9f0eab071462f22f097f05
Vansh Kashyap <kashvansh45@gmail.com>
2026-02-02 23:51:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
